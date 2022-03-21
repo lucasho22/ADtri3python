e52ec92f02612eaead3c3030232e903da6fb340d
Sahil Samar <64157584+AD1616@users.noreply.github.com>
2022-03-21 18:31:50 +0000

    Comments

--- pythonStuff/TT1/hacks.py
+++ pythonStuff/TT1/hacks.py
@@ -50,16 +50,19 @@ def print_data(n):
     print()
   
 def for_loop():
+    # calls print data function for n and iterates until all elements in list are covered
     for n in range(len(InfoDb)):
         print_data(n)
 
 def while_loop(n):
+    # calls print data function iteratively until it reaches the last element in list
     while n < len(InfoDb):
         print_data(n)
         n += 1
     return
 
 def recursive_loop(n):
+    # Calls itself until the if condition is not met
     if n < len(InfoDb):
         print_data(n)
         recursive_loop(n + 1)
@@ -68,24 +71,32 @@ def recursive_loop(n):
 # Hack 3
 
 def fib(n):
+  # Top two conditions there so that when 0 and 1 are reached, fib(n-1) + fib(n-2) can be computed
   if n == 0:
       return 0
   elif n == 1:
       return 1
   else:
-      return fib(n-1) + fib(n-2)
+      return fib(n-1) + fib(n-2) #This adds up the two numbers of the sequence before n
 
 def fibTester():
-  num = int(input("Enter a number for fibonnaci: "))
+  # Input for number of terms for fibonacci sequence 
+  num = int(input("Enter a number for fibonacci: "))
+
+  # Try/except used in case user types in a negative number or a non-number character
   try: 
     i = 0
     sequence = []
+    # While loop used to call the fib function for all values until and including the user input; this generates the entire sequence
     while (i <= num):
       sequence.append(fib(i))
       i +=1
     print("The sequence is", end = ' ')
+    # For loop is there so that the entire sequence is printed without the brackets
     for i in range(num):
       print(sequence[i+1], end = ', ')
+
+    # Prints out the final fib number separately if user is just looking to find the last fib number
     print("and the number", num , "fibonnaci number is", fib(num))
   except:
     print("...Sorry, please type in a positive integer.")
